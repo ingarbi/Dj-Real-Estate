25306adf466740e60e0b879d3ee6a4145569f0bb
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-04 23:22:09 +0300

    Pagination

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Listing
 
 
 def index(request):
+    listings = Listing.objects.select_related('realtor').order_by(
+        '-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': Listing.objects.all()
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context=context)
 
